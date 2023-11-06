cb85640dbe7d5096af08e21783802dde95a8b518
Alex Cândido <alex.candido.tec@gmail.com>
2023-11-06 11:55:55 -0300

    adicionado estrutura types em src/@types/

--- src/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,16 @@
-const DashboardPage = () => {
-  return <div>This is a Dashboard!</div>;
+interface DashboardPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  return (
+    <div>
+      This is a Dashboard!
+      <p>{params.storeId}</p>
+    </div>
+  );
 };
 
 export default DashboardPage;
